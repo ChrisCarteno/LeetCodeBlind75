44c35826db31f44bc1754337b88ec71157853624
Chrispy <christiancarteno@hotmail.com>
2023-09-07 23:49:13 -0700

    solution to 300 added

--- DynamicProgramming/longestIncSubseq.py
+++ DynamicProgramming/longestIncSubseq.py
@@ -11,4 +11,16 @@
 # Input: nums = [0,1,0,3,2,3]
 # Output: 4
 
+class Solution(object):
+    def lengthOfLIS(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        LIS = [1] * len(nums)
 
+        for i in range (len(nums) -1, -1, -1):
+            for j in range(i+1, len(nums)):
+                if nums[i] < nums[j]:
+                    LIS[i] = max(LIS[i], 1 + LIS[j])
+        return max(LIS)
