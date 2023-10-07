e5e00f1b4e2ce06fe5f64229ed96b666e1ba4ac2
Chrispy <christiancarteno@hotmail.com>
2023-10-06 23:51:45 -0700

    tree leve order traversal soulution

--- Tree/lvlOrdrTrvrsl.py
+++ Tree/lvlOrdrTrvrsl.py
@@ -31,4 +31,21 @@ class Solution(object):
         :type root: TreeNode
         :rtype: List[List[int]]
         """
+        if not root:
+            return []
         
+        queue = deque([root])
+        res = []
+        
+        while queue:
+            level = []
+            for _ in range(len(queue)):
+                node = queue.popleft()
+                level.append(node.val)
+                
+                if node.left:
+                    queue.append(node.left)
+                if node.right:
+                    queue.append(node.right)
+            res.append(level)
+        return res
