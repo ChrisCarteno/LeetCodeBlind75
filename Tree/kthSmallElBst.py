c4b4439743cdc6cfc188180c1b48a813c1b2c745
Chrispy <christiancarteno@hotmail.com>
2023-10-07 20:28:14 -0700

    changed name of problem

--- Tree/kthSmallElBst.py
+++ Tree/kthSmallElBst.py
@@ -1,23 +1,22 @@
-230. Kth Smallest Element in a BST
-=
-Given the root of a binary search tree, and an integer k, 
-return the kth smallest value (1-indexed) of all the values of 
-the nodes in the tree.
+# 230. Kth Smallest Element in a BST
+# Difficulty: Medium
+# Given the root of a binary search tree, and an integer k, 
+# return the kth smallest value (1-indexed) of all the values of 
+# the nodes in the tree.
 
+# Example 1:
+# Input: root = [3,1,4,null,2], k = 1
+# Output: 1
 
-Example 1:
-Input: root = [3,1,4,null,2], k = 1
-Output: 1
-
-Example 2:
-Input: root = [5,3,6,2,4,null,null,1], k = 3
-Output: 3
+# Example 2:
+# Input: root = [5,3,6,2,4,null,null,1], k = 3
+# Output: 3
  
-Constraints:
+# Constraints:
 
-The number of nodes in the tree is n.
-1 <= k <= n <= 104
-0 <= Node.val <= 104
+# The number of nodes in the tree is n.
+# 1 <= k <= n <= 104
+# 0 <= Node.val <= 104
  
 
 # Definition for a binary tree node.
