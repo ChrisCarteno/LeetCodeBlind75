c562e470f51ccfb9dadc69977c1026aa17fe1954
Chrispy <christiancarteno@hotmail.com>
2023-11-08 23:53:42 -0800

    course Schedule problem

--- Graph/crsSchdl.py
+++ Graph/crsSchdl.py
@@ -28,4 +28,28 @@
 #     All the pairs prerequisites[i] are unique.
 class Solution:
     def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
-        
+        # map each course to its prerequisites
+        courseDict = collections.defaultdict(list)
+        for relation in prerequisites:
+            nextCourse, prevCourse = relation[0], relation[1]
+            courseDict[nextCourse].append(prevCourse)
+
+        # visit each course
+        visited = set()
+        def dfs(course):
+            # return False if we are back to a course we've visited before
+            if course in visited:
+                return False
+            # return True if we've reached a course with no prerequisites
+            if courseDict[course] == []:
+                return True
+
+            # mark this course as visited
+            visited.add(course)
+            # visit all the prerequisites of this course
+            for prevCourse in courseDict[course]:
+                # if any of the prerequisites can't be visited, return False
+                if not dfs(prevCourse):
+                    return False
+            # all prerequisites can be visited, return True
+            return True
