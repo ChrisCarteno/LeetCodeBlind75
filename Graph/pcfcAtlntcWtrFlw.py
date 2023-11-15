0f2a205824a836c40687e5858ebc1bd35ce5e74d
Christian Carteno <95937411+ChrisCarteno@users.noreply.github.com>
2023-11-14 22:38:35 -0800

    Update pcfcAtlntcWtrFlw.py
    solution pacific atlantic flow

--- Graph/pcfcAtlntcWtrFlw.py
+++ Graph/pcfcAtlntcWtrFlw.py
@@ -42,48 +42,32 @@
 
 class Solution:
     def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
-        # get size of grid
-        m, n = len(heights), len(heights[0])
-        # create an array for each ocean
-        pacific = [[False] * n for _ in range(m)]
-        atlantic = [[False] * n for _ in range(m)]
+        rows = len(heights)
+        cols = len(heights[0])
+        pac, atl = set(), set()
         
-        # create queue for each ocean
-        pacificQueue = collections.deque()
-        atlanticQueue = collections.deque()
-        
-        # add cells in first and last row to queue
-        for i in range(m):
-            pacificQueue.append((i, 0))
-            atlanticQueue.append((i, n-1))
-            pacific[i][0] = True
-            atlantic[i][n-1] = True
+        def dfs(r, c, visit, prev_height):
+            if((r,c) in visit or r < 0 or r == rows or c < 0 or c == cols or heights[r][c] < prev_height):
+                return
+            visit.add((r,c))
+            dfs(r + 1, c, visit, heights[r][c])
+            dfs(r - 1, c, visit, heights[r][c])
+            dfs(r, c + 1, visit, heights[r][c])
+            dfs(r, c - 1, visit, heights[r][c])
             
-        # add cells in first and last column to queue
-        for i in range(n):
-            pacificQueue.append((0, i))
-            atlanticQueue.append((m-1, i))
-            pacific[0][i] = True
-            atlantic[m-1][i] = True
+            
+        for c in range(cols):
+            dfs(0, c, pac, heights[0][c])
+            dfs(rows - 1 , c, atl, heights[rows - 1][c])
+            
+        for r in range(rows):
+            dfs(r, 0, pac, heights[r][0])
+            dfs(r, cols - 1, atl, heights[r][cols - 1])
         
-        # run bfs on each ocean
-        self.bfs(heights, pacific, pacificQueue)
-        self.bfs(heights, atlantic, atlanticQueue)
+        res = []
         
-        # return list of cells that can flow to both oceans
-        result = []
-        for i in range(m):
-            for j in range(n):
-                if pacific[i][j] and atlantic[i][j]:
-                    result.append([i, j])
-        return result
-    
-    def bfs(self, heights, visited, queue):
-        m, n = len(heights), len(heights[0])
-        while queue:
-            x, y = queue.popleft()
-            for dx, dy in [(1,0), (-1,0), (0,1), (0,-1)]:
-                nx, ny = x + dx, y + dy
-                if 0 <= nx < m and 0 <= ny < n and not visited[nx][ny] and heights[nx][ny] >= heights[x][y]:
-                    visited[nx][ny] = True
-                    queue.append((nx, ny))
+        for r in range(rows):
+            for c in range(cols):
+                if (r,c) in pac and (r,c) in atl:
+                    res.append([r,c])
+        return res
